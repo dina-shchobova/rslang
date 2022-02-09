526efb81bee8f6501fcb78e177771f3281288c0a
Dina Shchobova <dina.shchobova@gmail.com>
2022-02-09 10:59:57 +0300

    feat: add audiocall to router

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import './style.scss';
 import { Authorization } from './authorization/authorization';
-
-const authorization = new Authorization();
-authorization.createFieldAuthorization();
-
 import { ApplicationRoute } from './services/application-route';
 import { Navbar } from './views/components/navbar';
 import { Home } from './views/pages/home';
 import { TextBook } from './views/pages/text-book';
 import { Games } from './views/pages/games';
 import { Statistics } from './views/pages/statistics';
+import './audiocall/styles/audiocallStyle.scss';
+import { Audiocall } from './audiocall/components/Audiocall';
+
+const authorization = new Authorization();
+authorization.createFieldAuthorization();
 
 const headerStart = document.createElement('header');
 const pageContainer = document.createElement('div');
@@ -29,17 +30,25 @@ const navAwaiter = async () => {
 navAwaiter();
 const content = document.getElementById('page_container') as HTMLDivElement;
 
+const AudioCallBinder = async (): Promise<string> => {
+  const main = document.body.querySelector('main') as HTMLElement;
+  main.innerHTML = '';
+  const audiocall = new Audiocall();
+  audiocall.mount(main);
+  return '';
+};
+
 const routes = {
   '/': Home,
   '/text-book': TextBook,
   '/games': Games,
+  '/games-audiocall': AudioCallBinder,
   '/winners': Statistics,
 };
 
 const notFound = async () => '<div>Not Found</div>';
 const app = new ApplicationRoute(content, routes, notFound);
 app.listen();
-
 
 const burger = document.querySelector('.menu') as HTMLElement;
 (function switchBurger() {
