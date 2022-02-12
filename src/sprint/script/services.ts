0ea0e8f4e656cfe25847d18238ec797785d64fc1
taleatg <t_korbyt@mail.ru>
2022-02-12 22:32:03 +0300

    feat: add new words count

--- src/sprint/script/services.ts
+++ src/sprint/script/services.ts
@@ -1,4 +1,4 @@
-import { DataWords, UserWords } from './dataTypes';
+import { DataWords } from './dataTypes';
 
 const path = 'https://rs-learnwords.herokuapp.com/';
 
@@ -10,30 +10,4 @@ export const getWord = async (group: number, page: number): Promise<[DataWords]>
     },
   });
   return response.json();
-};
-
-export const createUserWord = async (userId: string, wordId: string, word: object): Promise<[UserWords]> => {
-  const token = JSON.parse(<string>localStorage.getItem('user'))?.token;
-  const rawResponse = await fetch(`${path}users/${userId}/words/${wordId}`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(word),
-  });
-  return rawResponse.json();
-};
-
-export const getUserWord = async (userId: string, wordId: string): Promise<[UserWords]> => {
-  const token = JSON.parse(<string>localStorage.getItem('user'))?.token;
-  const rawResponse = await fetch(`${path}users/${userId}/words/${wordId}`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      Accept: 'application/json',
-    },
-  });
-  return rawResponse.json();
 };
