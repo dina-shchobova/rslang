f900291422e1b1cf9fcc5a3419dfe17548492663
Alexander Sivkov <sivkov@inbox.ru>
2022-03-08 12:37:44 +0300

    Delete src directory

--- src/views/pages/difficult-words.ts
+++
@@ -1,7 +0,0 @@
-import { PageComponentThunk } from '../../services/types';
-
-export const DifficultWords: PageComponentThunk = async () => ({
-  html: `<div class="word-cards-container">
-    </div>`,
-  mount: () => {},
-});
